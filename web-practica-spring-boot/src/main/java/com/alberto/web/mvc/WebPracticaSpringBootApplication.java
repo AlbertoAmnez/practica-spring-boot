b1dfeb73d4b8c893276e106847707f250c7fc205
Alberto Amores <AlbertoAmnez>
2024-05-07 09:20:34 +0200

    primer commit

---
+++ web-practica-spring-boot/src/main/java/com/alberto/web/mvc/WebPracticaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.alberto.web.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPracticaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPracticaSpringBootApplication.class, args);
+	}
+
+}
---
+++ web-practica-spring-boot/src/test/java/com/alberto/web/mvc/WebPracticaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.web.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPracticaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
