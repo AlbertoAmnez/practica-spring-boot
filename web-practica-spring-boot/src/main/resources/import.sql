f7e2c5b2240e32f3bed711f65b7262ef8278736e
Alberto Amores <AlbertoAmnez>
2024-05-09 15:00:34 +0200

    Creacion entidad Proyecto, services, controlador, dao y html.

--- web-practica-spring-boot/src/main/resources/import.sql
+++ web-practica-spring-boot/src/main/resources/import.sql
@@ -3,3 +3,10 @@ INSERT INTO alumno (nombre,apellido,telefono,email,dni,fecha_nac) VALUE ('Juan',
 INSERT INTO alumno (nombre,apellido,telefono,email,dni,fecha_nac) VALUE ('Alberto','Ruiz', 654213258, 'ar@gmail.com','C521236', '2024-05-07');
 INSERT INTO alumno (nombre,apellido,telefono,email,dni,fecha_nac) VALUE ('Rosa','Gracia', 674122135, 'rg@gmail.com','B728893', '2024-05-07');
 INSERT INTO alumno (nombre,apellido,telefono,email,dni,fecha_nac) VALUE ('Laura','Vizcaino', 624865231, 'lv@gmail.com','D841226', '2024-05-07');
+
+
+INSERT INTO proyecto (nombre,descripcion,fecha_inicio,fecha_fin,activo) VALUE ('Athletic', 'Proyecto deportivo', '2023-05-05', '2024-05-07', True);
+INSERT INTO proyecto (nombre,descripcion,fecha_inicio,fecha_fin,activo) VALUE ('Cinema', 'Catalogo de peliculas', '2021-03-02', '2024-05-09', True);
+INSERT INTO proyecto (nombre,descripcion,fecha_inicio,fecha_fin,activo) VALUE ('Clothes', 'Tienda online de ropa', '2020-05-05', '2024-01-20', False);
+INSERT INTO proyecto (nombre,descripcion,fecha_inicio,fecha_fin,activo) VALUE ('Eat Now', 'Comida a domicilio', '2024-01-05', '2024-02-12', True);
+INSERT INTO proyecto (nombre,descripcion,fecha_inicio,fecha_fin,activo) VALUE ('Teatro2024', 'Giras teatrales de 2024', '2024-01-01', '2024-12-31', False);
